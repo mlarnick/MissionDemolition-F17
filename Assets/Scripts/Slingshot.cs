a2b5ae573a606ec51a9ba01e4a0f074a4efb8abc
mlarnick <mlarnick@optonline.net>
2017-09-22 10:53:07 -0400

    Shos slingshot is active

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
 
+    public GameObject launchPoint;
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+
     void OnMouseEnter()
     {
         print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
     }
 
     void OnMouseExit()
     {
-        print("Slingshot:OnMouseExit()");    
+        print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
     }
 }
